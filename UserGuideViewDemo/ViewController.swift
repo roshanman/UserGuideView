9b74213a2dcfb9fa9a177061e02bf3da475e86ea
morenotepad@163.com <morenotepad@163.com>
2015-12-29 16:45:38 +0800

    Initial Commit

---
+++ UserGuideViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserGuideViewDemo
+//
+//  Created by DamonDing on 15/12/29.
+//  Copyright © 2015年 morenotepad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
